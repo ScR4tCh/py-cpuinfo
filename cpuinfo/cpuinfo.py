03e27603a97e61aceb728c3c51abd80c7b3662cb
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-09-12 16:26:30 -0700

    Added monkey patch to hopefully fix forking under pyinstaller.

--- cpuinfo/cpuinfo.py
+++ cpuinfo/cpuinfo.py
@@ -1,5 +1,44 @@
-
 import sys
+import platform
+
+
+# https://github.com/pyinstaller/pyinstaller/wiki/Recipe-Multiprocessing
+if sys.platform.startswith('win'):
+    print("Applying Windows hacks ..."); sys.stdout.flush()
+    # Module multiprocessing is organized differently in Python 3.4+
+    try:
+        # Python 3.4+
+        if sys.platform.startswith('win'):
+            import multiprocessing.popen_spawn_win32 as forking
+        else:
+            import multiprocessing.popen_fork as forking
+    except ImportError:
+        import multiprocessing.forking as forking
+
+    # First define a modified version of Popen.
+    class _Popen(forking.Popen):
+        def __init__(self, *args, **kw):
+            if hasattr(sys, 'frozen'):
+                # We have to set original _MEIPASS2 value from sys._MEIPASS
+                # to get --onefile mode working.
+                os.putenv('_MEIPASS2', sys._MEIPASS)
+            try:
+                super(_Popen, self).__init__(*args, **kw)
+            finally:
+                if hasattr(sys, 'frozen'):
+                    # On some platforms (e.g. AIX) 'os.unsetenv()' is not
+                    # available. In those cases we cannot delete the variable
+                    # but only set it to the empty string. The bootloader
+                    # can handle this case.
+                    if hasattr(os, 'unsetenv'):
+                        os.unsetenv('_MEIPASS2')
+                    else:
+                        os.putenv('_MEIPASS2', '')
+
+    # Second override 'Popen' class with our modified version.
+    forking.Popen = _Popen
+
+import multiprocessing
 
 print("""
 !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
@@ -54,6 +93,5 @@ def main():
 
 
 if __name__ == '__main__':
-	from multiprocessing import freeze_support
-	freeze_support()
+	multiprocessing.freeze_support()
 	main()
